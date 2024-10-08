4232a510321372e13afd28ea99134c5b299fcc06
fasti314 <fabian.stirnberg001@stud.fh-dortmund.de>
2024-10-08 09:02:28 +0200

    init

---
+++ PrimzahlenMain.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+
+public class PrimzahlenMain
+{
+
+    public static final int MAXPRIM = 1000000;
+    
+    public static void main(String[] args)
+    {
+  
+        int n;
+        Primzahlen meinePrimzahlen = new Primzahlen(MAXPRIM);
+        Scanner sc = new Scanner(System.in);
+
+        PrimzahlenConsumer pc = new PrimzahlenConsumer(meinePrimzahlen); 
+        PrimzahlenProducer pp = new PrimzahlenProducer(meinePrimzahlen); 
+        do
+        {
+            // Einlesen einer Zahl
+            n = sc.nextInt();
+            if(n<MAXPRIM)
+            {
+                if(pc.istPrimzahl(n))
+                { 
+                    System.out.println(n + " ist eine Primzahl");
+                }
+                else
+                { 
+                    System.out.println(n + " ist keine Primzahl");
+                }
+            }
+        }   
+        while(n>0);
+    }
+}
+    
